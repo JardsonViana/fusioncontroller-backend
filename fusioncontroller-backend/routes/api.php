7fdda36d2ed783de4ee478b97e9d07ab2e12840c
JardsonViana <jardson.consultoria@gmail.com>
2024-03-11 17:48:48 -0300

    Adicionado rotas de auth e o controller de auth

--- fusioncontroller-backend/routes/api.php
+++ fusioncontroller-backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\JsonResponse;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +16,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+/* Rotas de utilidade: */
+Route::get('/ping', function(): JsonResponse {
+    return response()->json(['Pong' => true]);
 });
 
+/* Rotas de Autenticação * Autenticação via TOKEN */
+Route::post('/user/signin', [AuthController::class, 'login']);
+Route::post('/user/new', [AuthController::class, 'newUsuario']);
+Route::get('/user', [AuthController::class, 'usuario']);
+Route::get('/user/logout', [AuthController::class, 'logout']);
+
+/**
+ * Rota de utilidade
+ * [ ] - /ping - Responde PONG
+ * 
+ *  - Rotas de Autenticação * Autenticação via TOKEN
+ * [ ] - /user/signin -- Login
+ * [ ] - /user/new    -- Cadastro de usuario no sistema
+ * [ ] - /user        -- Informação do usuario
+ * 
+ * - Rotas de clientes
+ * [ ] - /client/list - Listar todos os clientes cadastrados
+ * [ ] - /client/:id  - Dados de um cliente especifico
+ * [ ] - /client/new  - Adicionar um cliente novo
+ * [ ] - /client/:id(PUT) - Alterar um cliente cadastrado
+ * [ ] - /client/:id(DELETE) - Deletar um cliente cadastrado
+ * 
+ * - Rotas de Provedores
+ * [ ] - /provider/:id - Dados de um provider especifico
+ * [ ] - /provider/new - Adicionar um provider
+ * [ ] - /provider/:id(PUT) - Alterar um provider
+ * [ ] - /provider/:id(del) - Deleter um provider
+ * 
+ * - Rotas de Circuito
+ * [ ] - ??
+ * 
+ * - Rotas de Equipaments
+ * [ ] - /equipament/list - Lista todos os equipamentos homologados
+ * [ ] - /equipament/new  - Cadastra um novo equipamento
+ * [ ] - /equipament/:id  - Dados de um equipamento
+ * 
+ * - Rotas de CPE
+ * [ ] - /cpe/list - Listar todas as CPEs
+ * [ ] - /cpe/new  - Adicionar nova CPE
+ * [ ] - /cpe/:id  - Dados de uma CPE
+ * [ ] - /cpe/:id(PUT) - Alterar CPE
+ * [ ] - /cpe/:id(del) - Deletar CPE
+ * 
+ * - Rotas de configuração 
+ * [ ] - /config/new - Gerar nova config
+ * [ ] - /config/list - Visualizar a config
+ * [ ] - /config/send - Enviando config
+ * [ ] - /config/status - Validando config
+ * 
+ * 
+ * 
+ */
